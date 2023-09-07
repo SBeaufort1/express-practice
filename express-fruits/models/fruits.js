6bb9be8468e18761fe212fc2600363ecef75244b
sheba beaufort <shebabeaufort@gmail.com>
2023-09-06 20:52:57 -0400

    added .jsv files and fruits/veggie.js

---
+++ express-fruits/models/fruits.js
@@ -0,0 +1,20 @@
+// Create array of fruits 
+const fruits =[
+  {
+   name:'Apple',
+   color: 'red',
+   readyToEat: true
+  }, 
+  {
+   name:'Banana',
+   color: 'yellow',
+   readyToEat: true
+  }, 
+  {
+   name:'Pear',
+   color: 'green',
+   readyToEat: false
+  } 
+  ];
+
+  module.exports = fruits;
---
+++ express-fruits/models/veggies.js
@@ -0,0 +1,19 @@
+const vegatables = [
+  {
+    name: "spinach",
+    color: "green",
+    readyToEat: true,
+  },
+  {
+    name: "onion",
+    color: "yellow",
+    readyToEat: true,
+  },
+  {
+    name: "eggplant",
+    color: "purple",
+    readyToEat: false,
+  }
+];
+
+module.exports = vegatables;
